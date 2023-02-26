d441c58b66a2464b935add07a744e71f3557da7d
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-02-26 16:53:55 +0000

    Add files via upload

---
+++ Cost_area.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+  int s = 14 ;
+  int area = s * s ;
+  int cost = 7 * area ;
+  printf("%d \n", area );   //Area and cost have to be output on separate lines
+  printf("%d", cost );      //'$' has to be printed after cost without any space
+  printf("$"); 
+  return 0;
+}
